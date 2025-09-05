f608869c3368def7c7d0f3b4a885e3ef3cff668d
Esmeralda-Amador <124049384@upq.edu.mx>
2025-09-04 22:56:45 -0600

    agregamos reglamento y lineamientos al codigo

---
+++ ACT 2/InicioPAM.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package iniciopam;
+import java.util.Scanner;
+/**
+ *
+ * @author Esme Garduño
+ */
+public class InicioPAM {
+
+    public String reglamentoPOO(){
+    return "---REGLAMENTO--\n"  
+            + "1. Se requiere 80% de asistencia para tener derecho a evaluación parcial y 80% de trabajos en clase.\n"
+             + "2. Se permiten 10 minutos de tolerancia y si el alumno llega después de este tiempo puede permanecer en la clase, "
+             + "pero no se tomará la asistencia (Solamente en los horarios de inicio: 7:00 a.m y 14:00 p.m).\n"
+             + "3. Las faltas deberán estar justificadas mediante el correo institucional con un plazo máximo de 24 horas posteriores "
+             + "a la hora de falta en clase mediante correo del tutor(a). Justificantes entregados fuera de la fecha límite permitido "
+             + "no se aceptan, únicamente se aceptarán recetas médicas y citatorios jurídicos (De forma física deben ser presentados "
+             + "al tutor para ser validados y de forma posterior emitidos).\n"
+             + "4. Las tareas y trabajos deberán subirse al Classroom de forma individual y no se recibirán de manera extemporánea "
+             + "(salvo previo justificante validado por el tutor).\n"
+             + "5. Las tareas y trabajos deben presentarse en tiempo y forma. La demora sin justificante o con justificante fuera del límite no se acepta.\n"
+             + "6. Utilizar el correo institucional para trabajar bajo la plataforma Google Classroom.\n"
+             + "7. El plagio o copia de trabajos y/o exámenes, será condicionado a reprobar la asignatura y se reportará al área correspondiente.\n"
+             + "8. Cualquier deshonestidad académica será sancionada reprobando el parcial sin derecho a examen final.\n"
+             + "9. En caso de indisciplina o falta de respeto hacia docentes, administrativos, compañeros o cualquier persona de la universidad, "
+             + "se realizará una primera llamada de atención. Si el alumno hace caso omiso tendrá que abandonar el aula. "
+             + "Tres incidencias de este tipo implican perder derecho a examen final o parcial.\n"
+             + "10. Uso de laptops y/o dispositivos móviles quedará limitado a las actividades que lo requieran.\n"
+             + "11. Prohibido el uso de audífonos durante la clase.\n"
+             + "12. Prohibido comer y/o tomar líquidos en el salón.\n"
+             + "13. Prohibido sentarse encima de las mesas, así como columpiarse en las sillas.\n"
+             + "14. Todo tema académico debe ser revisado primero con el docente; de no resolverse, pasar con el tutor; si continúa, "
+             + "con la coordinación de tutores. Finalmente, si no se soluciona, con la dirección del programa educativo.\n"
+             + "15. Cualquier situación no prevista en este reglamento pasará directamente con la dirección del programa educativo.\n"
+             + "16. El día destinado a entrega de calificaciones todos los estudiantes deben estar presentes. Ese día se entregarán exámenes y se brindará retroalimentación.\n"
+             + "17. Este reglamento entra en vigor después de que se firme o se acepte por la mayoría de los estudiantes asistentes a la primera sesión de la materia. "
+             + "Una vez firmado o aceptado por el 50% más el jefe de grupo, es vigente para todo alumno inscrito aunque no esté presente en la primera sesión.\n";
+ 
+    }
+    public String lineamientosclassroom(){
+        return "LINEAMIENTOS\n"
+        +"1. Evidencia de conocimiento(40%), Evidencia de desempeño(20%), Evidencia de producto(30%), Proyecto integrador(10%)\n"
+            + " 2. Evidencia de conocimiento(40%), Evidencia de desempeño(20%), Evidencia de producto(20%), Proyecto integrador(20%)\n"
+             + " 3. Evidencia de conocimiento(20%), Evidencia de desempeño(10%), Evidencia de producto(40%), Proyecto integrador(30%)\n";
+    }
+    
+}
