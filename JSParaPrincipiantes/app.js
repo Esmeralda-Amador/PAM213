be3713c2b09fd9dd1dd36958a4b6487e010c6b76
Esmeralda-Amador <124049384@upq.edu.mx>
2025-09-10 11:45:12 -0600

    Ejercicios con if,este

--- JSParaPrincipiantes/app.js
+++ JSParaPrincipiantes/app.js
@@ -1,13 +1,10 @@
-let passwordDB= 'pepe123'
-let input= 'pep3'
-let result = input == passwordDB;
-
-if (result=== true ){
-    console.log('login correcto');
-
+let score=70;
+if (score>30){
+    console.log('You need to practice more');
 }
-if (result=== false ){
-    console.log('login incorrecto');
-
+if (score>15){
+    console.log('EStas mejorandop');
+    
+}else{
+    console.log('Necesitas seguir este tutorial');
 }
-
