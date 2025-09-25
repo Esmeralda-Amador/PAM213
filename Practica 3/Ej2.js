226c24e2b9755eec0684105266a67d5c97bcf643
Esmeralda-Amador <124049384@upq.edu.mx>
2025-09-25 15:14:07 -0600

    Realizamos el ejercicio 3,usando try,catch,await,etc

---
+++ Practica 3/Ej2.js
@@ -0,0 +1,16 @@
+function simularPeticionAPI(){
+    return new Promise(resolve=>{
+        setTimeout(() =>{
+            resolve('Datos recibidos correctamente');
+        }, 5000);
+    });
+}
+async function obtenerDatos(){
+     try {
+        const respuesta =await (simularPeticionAPI);
+        console.log("Resultado de la petición",respuesta);
+     }catch(error){
+        console.log("Error", error.message);
+     }
+}
+obtenerDatos();
