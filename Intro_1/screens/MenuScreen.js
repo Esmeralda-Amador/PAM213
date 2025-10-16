b31ae8afa59510f48faef8d33316e14dd3d90665
Esmeralda-Amador <124049384@upq.edu.mx>
2025-10-15 19:21:54 -0600

    Importamos nuevas carpetas

---
+++ Intro_1/screens/MenuScreen.js
@@ -0,0 +1,15 @@
+import { Text, StyleSheet, View } from 'react-native'
+import React, { useSate } from 'react'
+import contadorScreen from './contadorScreen'
+import BotonesScreen from './BotonesScreen'
+
+export default function MenuScreen() {
+
+    return (
+      <View>
+        <Text> textInComponent </Text>
+      </View>
+    )
+  }
+
+const styles = StyleSheet.create({})
