bb161fa7c88704754e4f7000dc3669033ea5f989
Esmeralda-Amador <124049384@upq.edu.mx>
2025-10-15 22:13:18 -0600

    Modificamos las propiedades de nuestro color,letras,etc
    corregí un boton

---
+++ Intro_1/screens/ActivityIndicator.js
@@ -0,0 +1,12 @@
+
+import { Text, StyleSheet, View } from 'react-native'
+
+export default function ActivityIndicator() {
+    return (
+      <View>
+        <Text> Prox... </Text>
+      </View>
+    )
+}
+
+const styles = StyleSheet.create({})
--- Intro_1/screens/MenuScreen.js
+++ Intro_1/screens/MenuScreen.js
@@ -19,7 +19,7 @@ export default function MenuScreen() {
         return <ContadorScreen/>;
       case 'botones':
         return <BotonesScreen />;
-      case 'text Input':
+      case 'textInput':
         return <TextInput/>;
       case 'imageBackgroung':
         return <ImageBackgroung/>;
@@ -31,23 +31,50 @@ export default function MenuScreen() {
         return <FlatList/>;
       case 'modal':
         return <Modal/>;
-      case 'bottom Sheet':
+      case 'buttomSheet':
         return <ButtomSheet/>;
       case 'menu':
             default:
                 return (
-                    <View >
-                        <Text> Menú de Prácticas</Text>
-                        <Button title='Pract: Contador' onPress={()=>setScreen('contador')}/>{/* el valor que esté dentro de setScreen debe ser igual al del switch */}
-                        <Button title='Pract: Buttons' onPress={()=>setScreen('botones')}/>
-                        <Button title='Pract: TextInput' onPress={()=>setScreen('textInput')}/>
-                        <Button title='Pract: ImageBackgroung' onPress={()=>setScreen('imageBackgroung')}/>
-                        <Button title='Pract: ScrollView' onPress={()=>setScreen('scrollView')}/>
-                        <Button title='Pract: Activity' onPress={()=>setScreen('activityIndicator')}/>
-                        <Button title='Pract: FlatList' onPress={()=>setScreen('flatList')}/>
-                        <Button title='Pract: Modal' onPress={()=>setScreen('modal')}/>
-                        <Button title='Pract: Buttom Sheet' onPress={()=>setScreen('buttomSheet')}/>
+                    <View  style={styles.container}>
+                        <Text style={styles.texto} > Menú de Prácticas</Text>
+                        <View style={styles.botonesContainer}>
+                        <Button title='Pract: Contador' onPress={()=>setScreen('contador')} color ='#ff26a8ff'  />{/* el valor que esté dentro de setScreen debe ser igual al del switch */}
+                        <Button title='Pract: Buttons' onPress={()=>setScreen('botones')} color ='#ff26a8ff' />
+                        <Button title='Pract: TextInput' onPress={()=>setScreen('textInput')}color ='#ff26a8ff' />
+                        <Button title='Pract: ImageBackgroung' onPress={()=>setScreen('imageBackgroung')}color ='#ff26a8ff'/>
+                        <Button title='Pract: ScrollView' onPress={()=>setScreen('scrollView')}color ='#ff26a8ff'/>
+                        <Button title='Pract: Activity' onPress={()=>setScreen('activityIndicator')}color ='#ff26a8ff'/>
+                        <Button title='Pract: FlatList' onPress={()=>setScreen('flatList')}color ='#ff26a8ff'/>
+                        <Button title='Pract: Modal' onPress={()=>setScreen('modal')}color ='#ff26a8ff'/>
+                        <Button title='Pract: Buttom Sheet' onPress={()=>setScreen('buttomSheet')}color ='#ff26a8ff'/>
+                         </View> 
                     </View>
                 )
     }
   }
+  const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffc4d6ff',
+    alignItems: 'center', //posicionamiento sobre x
+    justifyContent: 'center', //posicionamiento sobre y- 
+    gap: 5.5,
+    
+  },
+  texto:{
+     color: '#ffffffff',
+     fontSize: 45,
+     fontFamily: 'Times New Roman',
+     fontWeight:'bold',
+     fontStyle: 'italic',
+     //textDecorationLine: 'line-through',
+  },
+   botonesContainer:{
+    marginTop :20,
+    //flexDirection:'row',
+    alignItems: 'center', 
+    //justifyContent: 'center',
+    gap:15,
+  },
+  });
