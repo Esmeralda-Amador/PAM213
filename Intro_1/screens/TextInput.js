c50a100679fcfe522f9b3e6b668d034ca05d3abc
Esmeralda-Amador <124049384@upq.edu.mx>
2025-10-21 17:28:07 -0600

    TextInput & Alert: Completada

--- Intro_1/screens/TextInput.js
+++ Intro_1/screens/TextInput.js
@@ -1,29 +1,93 @@
+import { Text, StyleSheet, View, Button, Alert, Platform, TextInput } from 'react-native'
+import React,{useState} from 'react'
+//realizamos las importaciones necesarias
 
-import { Text, StyleSheet, View } from 'react-native'
+export default function textInput() {
+    const [nombre, setNombre]= useState('');
+    const[contraseña, setContraseña]= useState('');
+    const[multexto, setMultitexto]= useState('');
+    const mostrarAlerta=()=>{
+        if (nombre.trim()===''){
+            if(Platform.OS==='web'){
+                alert('Por favor escribe tu nombre antes de continuar.');
+    }else{
+        Alert.alert(
+            'Atención',
+            'Por favor, escribe tu nombre antes de continuar.',
+            [
+                {text: 'cancelar'},
+                {text: 'aceptar'}
+            ]
+        );
+    }
 
-export default function TextInput() {
-    return (
-      <View style={styles.container}>
-         <Text style={styles.texto} > Proximamente.... </Text>
-       </View>
-    )
-  }
+  }else{
+    if(Platform.OS==='web'){
+        alert(`Bienvenido, ${nombre}!`);
+    }else{
+        Alert.alert('Hola',`Bienvenido, ${nombre}!`,
+            [
+                {text:'cancelar'},
+                {text:'aceptar'}
+            ]
+        );
+      }
+    }
+  };
+return (
+    <View style={styles.container}>
+        <Text style={styles.titulo}>Práctica: TextInput y Alert</Text>
+        <TextInput
+            style={styles.input}
+            placeholder='Escribe tu nombre'
+            value={nombre}
+            onChangeText={setNombre}
+        />
 
- const styles = StyleSheet.create({
+        <TextInput
+            style={styles.input}
+            placeholder='Escribe tu contraseña'
+            secureTextEntry={true}
+            keyboardType='numeric'
+            value={contraseña}
+            onChangeText={setContraseña}
+        />
+
+        <TextInput
+            style={styles.input}
+            placeholder='Escribe tu nombre'
+            value={multexto}
+            multiline={true}
+            onChangeText={setMultitexto}
+        />
+        <Button title="Mostrar Alerta" onPress={mostrarAlerta} color ='#ff26a8ff' />
+    </View>
+);
+
+}
+
+const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
     backgroundColor: '#ffc4d6ff',
-    alignItems: 'center', //posicionamiento sobre x
-    justifyContent: 'center', //posicionamiento sobre y- 
-    gap: 5.5,
-    
+    padding: 20,
   },
-  texto:{
-     color: '#ffffffff',
-     fontSize: 45,
-     fontFamily: 'Times New Roman',
-     fontWeight:'bold',
-     fontStyle: 'italic',
-     //textDecorationLine: 'line-through',
-  }
-})
+  titulo:{
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginBottom: 15,
+    color: '#ffffffff',
+    fontFamily: 'Times New Roman',
+    fontStyle: 'italic',
+  },
+    input:{
+        borderWidth: 1,
+        borderColor: '#ffffffff',
+        borderRadius: 5,
+        width: '80%',
+        padding: 10,
+        marginBottom: 15,
+    },
+  });
